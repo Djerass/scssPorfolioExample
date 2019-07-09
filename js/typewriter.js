b0469195d187276bde4b916b9315dfe4b3c11467
Djerass <dsaraula@yandex.ru>
2019-07-09 22:22:19 +0300

    Added TextWriter Script

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,65 +1,53 @@
-class TypeWriter {
-  constructor(txtElement, words, wait = 3000) {
-    this.txtElement = txtElement;
-    this.words = words;
-    this.txt = '';
+class TextWriter {
+  constructor(selector) {
+    let temp = document.querySelector(selector);
+    this.output = temp;
+    this.words = JSON.parse(temp.getAttribute("data-words")); // Array of Strings
+    this.delay = Number.parseInt(temp.getAttribute("data-delay")); // Delay number
+    this.deleteDelay = this.delay / 2;
+    this.letterIndex = 0;
     this.wordIndex = 0;
-    this.wait = parseInt(wait, 10);
+    this.word = this.words[this.wordIndex];
     this.type();
-    this.isDeleting = false;
   }
-
   type() {
-    // Current index of word
-    const current = this.wordIndex % this.words.length;
-    // Get full text of current word
-    const fullTxt = this.words[current];
-
-    // Check if deleting
-    if (this.isDeleting) {
-      // Remove char
-      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    // Type letter of the word, if previous letter was last start deleting
+    if (this.letterIndex < this.word.length) {
+      setTimeout(() => this._addLetter(), this.delay);
     } else {
-      // Add char
-      this.txt = fullTxt.substring(0, this.txt.length + 1);
+      setTimeout(() => this.remove(), this.delay);
     }
+  }
+  remove() {
+    //Remove letter
+    setTimeout(() => this._removeLetter(), this.deleteDelay);
+  }
 
-    // Insert txt into element
-    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
-
-    // Initial Type Speed
-    let typeSpeed = 300;
-
-    if (this.isDeleting) {
-      typeSpeed /= 2;
+  _addLetter() {
+    this.output.textContent += `${this.word[this.letterIndex]}`;
+    this.letterIndex++;
+    this.type();
+  }
+  _removeLetter() {
+    // Delete letters until end of the word, then move to a next word
+    const text = this.output.textContent;
+    if (text.length > 0) {
+      this.output.textContent = text.slice(0, text.length - 1);
+      this.remove();
+    } else {
+      this.letterIndex = 0;
+      if (this.wordIndex === this.words.length - 1) {
+        this.wordIndex = 0;
+      } else {
+        this.wordIndex++;
+      }
+      this.word = this.words[this.wordIndex];
+      this.type();
     }
-
-    // If word is complete
-    if (!this.isDeleting && this.txt === fullTxt) {
-      // Make pause at end
-      typeSpeed = this.wait;
-      // Set delete to true
-      this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-      this.isDeleting = false;
-      // Move to next word
-      this.wordIndex++;
-      // Pause before start typing
-      typeSpeed = 500;
-    }
-
-    setTimeout(() => this.type(), typeSpeed);
   }
 }
 
-// Init On DOM Load
-document.addEventListener('DOMContentLoaded', init);
-
-// Init App
-function init() {
-  const txtElement = document.querySelector('.txt-type');
-  const words = JSON.parse(txtElement.getAttribute('data-words'));
-  const wait = txtElement.getAttribute('data-wait');
-  // Init TypeWriter
-  new TypeWriter(txtElement, words, wait);
-}
+document.addEventListener(
+  "DOMContentLoaded",
+  () => new TextWriter("#txt-type")
+);
